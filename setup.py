d0f5273525dc143956c845d41affc85478db41a9
fenixinvitado2021 <104691589+fenixinvitado2021@users.noreply.github.com>
2023-04-24 00:23:33 -0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,26 +3,43 @@ import os
 
 from setuptools import setup, find_packages
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "revistas", "version.py")) as fp:
+    exec(fp.read())
+
+requires = []
+with open(os.path.join(here, "requirements.txt"),'r') as fp:
+    requires = str(fp.read()).split('\n'
+
 setup(
     name='revistas',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=[
-        'requests',
-        'bs4'
-    ],
+    version=__version__,
+    packages=["revistas"],
+    install_requires=requires,
     description='Client to Libraries',
     author='RayServer',
+    package_data={"": ["LICENSE"],},
     author_email='susej.mabel2009@gmail.com',
+    license="The Unlicense (Unlicense)",
     url='https://github.com/fenixinvitado2021/revistas',
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
     ],
+    zip_safe=True,
+    python_requires=">=3.6",
+    keywords=["telegram", "download", "stream", "bots",],
 )
